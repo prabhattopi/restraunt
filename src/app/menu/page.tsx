d85fd076cfb65c7ca007772101236ef8d2e05177
prabhattopi <topi9864@gmail.com>
2023-08-10 18:40:52 +0000

    the new changes

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,11 +1,10 @@
 import { menu } from "@/data";
 import Link from "next/link";
 import React from "react";
-import {Menu} from "@/data"
 const MenuPage = () => {
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
-      {menu.map((category:Menu) => (
+      {menu.map((category) => (
         <Link
           href={`/menu/${category.slug}`}
           key={category.id}
