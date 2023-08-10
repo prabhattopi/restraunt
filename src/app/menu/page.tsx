2cab97f3f176acffcac6818233afe60f35417c1d
prabhattopi <topi9864@gmail.com>
2023-08-10 18:28:19 +0000

    the new changes

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,7 @@
 import { menu } from "@/data";
 import Link from "next/link";
 import React from "react";
-import Menu from "@/data"
+import {Menu} from "@/data"
 const MenuPage = () => {
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
